9a68d66b7d0683285bbe358eaaaff505c8dcc712
zoldzebra <zoldzebra@gmail.com>
2018-05-10 13:02:33 +0200

    .gitignore and HelloWorld

---
+++ week-01/day-04/src/HelloDani.java
@@ -0,0 +1,8 @@
+public class HelloDani {
+
+    public static void main(String[] args) {
+        // Modify this program to greet you instead of the World!
+        System.out.println("Hello, Dani!");
+    }
+    
+}
