4a42ff93ad316d62d3163330253a6c9c8113b92d
zoldzebra <zoldzebra@gmail.com>
2018-05-11 10:58:28 +0200

    Init(DrawPyramid)

---
+++ week-01/day-04/src/DrawPyramid.java
@@ -0,0 +1,36 @@
+// Write a program that reads a number from the standard input, then draws a
+// pyramid like this:
+//
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as the number was
+
+
+import java.util.Scanner;
+
+public class DrawPyramid {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How tall pyramid do you wish for, oh pharaoh?");
+        int triangleRows = scanner.nextInt();
+
+        for (int row = 0; row < triangleRows+1; row++) {
+            for (int col = 0; col < ((row*2)-1); col++) {
+                for (int space = triangleRows; space < 0; space-- ) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+    }
+
+}
