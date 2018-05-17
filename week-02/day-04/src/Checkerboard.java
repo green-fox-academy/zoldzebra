b544446d6b1d05882deeba293dd658d434c2c1d7
zoldzebra <zoldzebra@gmail.com>
2018-05-17 23:01:58 +0200

    Checkerboard overcomplicatedly done.

--- week-02/day-04/src/Checkerboard.java
+++ week-02/day-04/src/Checkerboard.java
@@ -6,36 +6,33 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Checkerboard {
   public static void mainDraw(Graphics graphics) {
-    // Reproduce this:
-    // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
-    int[] squareData = {0,0,120,120,120,30}; // 0 = pos x, 1 = pos y 2-4 color, 5 size
+
+    int[] squareData = {0,0,255,30}; // 0 = pos x, 1 = pos y,  2 color, 3 size
     int squares = 64;
     for (int i = 0; i < squares ; i++) {
+      if ((((i / 8) % 2 == 0) && (i % 2 ==0)) || ((i % 2 != 0) && (((i-1) / 8) % 2 == 1))){
+        squareData[2] = 255;
+      } else {
+        squareData[2] = 0;
+      }
       drawSquare(squareData, graphics);
       squareDataCounter(squareData);
     }
   }
 
   public static void drawSquare (int[] squareData, Graphics graphics){
-    graphics.setColor(new Color(squareData[2], squareData[3], squareData[4]));
-    graphics.fillRect(squareData[0], squareData[1], squareData[5], squareData[5]);
+    graphics.setColor(new Color(squareData[2], squareData[2], squareData[2]));
+    graphics.fillRect(squareData[0], squareData[1], squareData[3], squareData[3]);
     graphics.setColor(Color.BLACK);
-    graphics.drawRect(squareData[0], squareData[1], squareData[5], squareData[5]);
+    graphics.drawRect(squareData[0], squareData[1], squareData[3], squareData[3]);
   }
 
-  public static int[] squareDataCounter (int[] squareData){
-    squareData[0] += squareData[5];
-    if (squareData[0] > squareData[5]*7) {
-      squareData[1] += squareData[5];
+  public static int[] squareDataCounter (int[] squareData) {
+    squareData[0] += squareData[3];
+    if (squareData[0] > squareData[3] * 7) {
+      squareData[1] += squareData[3];
       squareData[0] = 0;
     }
-    if ((squareData[1] % squareData[5]) == 1) {
-      squareData[2] = 255;
-      squareData[3] = 255;
-      squareData[4] = 255;
-    } else {
-
-    //}
     return squareData;
   }
 
