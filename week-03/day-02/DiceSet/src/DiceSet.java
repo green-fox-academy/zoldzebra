2880d4a45f1c2a9a292e876b3efc24dd5d26a365
zoldzebra <zoldzebra@gmail.com>
2018-05-23 14:25:58 +0200

    OOP Exercise4 DiceSet

---
+++ week-03/day-02/DiceSet/src/DiceSet.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class DiceSet {
+  // You have a `DiceSet` class which has a list for 6 dices
+// You can roll all of them with roll()
+// Check the current rolled numbers with getCurrent()
+// You can reroll with reroll()
+// Your task is to roll the dices until all of the dices are 6
+
+
+  int[] dices = new int[6];
+
+  public int[] roll() {
+    for (int i = 0; i < dices.length; i++) {
+      dices[i] = (int) (Math.random() * 6) + 1;
+    }
+    return dices;
+  }
+
+  public int[] getCurrent() {
+    return dices;
+  }
+
+  public int getCurrent(int i) {
+    return dices[i];
+  }
+
+  public void reroll() {
+    for (int i = 0; i < dices.length; i++) {
+      dices[i] = (int) (Math.random() * 6) + 1;
+    }
+  }
+
+  public void reroll(int k) {
+    dices[k] = (int) (Math.random() * 6) + 1;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + dices[0] + ", " + dices[1] + dices[2] + dices[3] + dices[4] + dices[5] + "]";
+  }
+
+
+}
---
+++ week-03/day-02/DiceSet/src/Dice_main.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Dice_main {
+
+  public static void main(String[] args) {
+    int rerolls = 0;
+
+    DiceSet diceSet = new DiceSet();
+    diceSet.roll();
+    System.out.println("Original roll: " + Arrays.toString(diceSet.dices));
+
+
+
+    for (int i = 0; i < diceSet.dices.length; i++) {
+      while (diceSet.getCurrent()[i] != 6){
+        diceSet.reroll(i);
+        rerolls ++;
+      }
+    }
+
+    System.out.println("Finally, after a total of " + rerolls + " single dice rerolls: " + Arrays.toString(diceSet.dices));
+
+
+    /*
+    diceSet.getCurrent();
+    diceSet.getCurrent();
+    diceSet.getCurrent(5);
+    diceSet.reroll();
+    diceSet.getCurrent();
+    diceSet.reroll(4);
+    diceSet.getCurrent();
+    */
+  }
+
+}
