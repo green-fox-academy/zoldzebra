3e1ab266a924d886a76bde4f986760b771ca4606
zoldzebra <zoldzebra@gmail.com>
2018-05-23 12:57:13 +0200

    OOP exercise2 (BlogPost)

---
+++ week-03/day-02/Blogpost/src/Blogpost.java
@@ -0,0 +1,20 @@
+/*an authorName
+a title
+a text
+a publicationDate
+*/
+
+public class Blogpost {
+  String authorName;
+  String title;
+  String text;
+  String publicationDate;
+
+  public Blogpost(String authorName, String title, String text, String publicationDate){
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicationDate = publicationDate;
+  }
+
+}
---
+++ week-03/day-02/Blogpost/src/Blogpost_main.java
@@ -0,0 +1,28 @@
+/*Create a few blog post objects:
+"Lorem Ipsum" titled by John Doe posted at "2000.05.04."
+Lorem ipsum dolor sit amet.
+"Wait but why" titled by Tim Urban posted at "2010.10.10."
+A popular long-form, stick-figure-illustrated blog about almost everything.
+"One Engineer Is Trying to Get IBM to Reckon With Trump" titled by William Turton at "2017.03.28."
+Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention.
+When I asked to take his picture outside one of IBM’s New York City offices,
+he told me that he wasn’t really into the whole organizer profile thing.
+*/
+
+public class Blogpost_main {
+
+  public static void main(String[] args) {
+
+    Blogpost post0 = new Blogpost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet." , "2000.05.04.");
+    Blogpost post1 = new Blogpost("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything." , "2010.10.10.");
+    Blogpost post2 = new Blogpost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. \n" +
+            "When I asked to take his picture outside one of IBM’s New York City offices, \n" +
+            "he told me that he wasn’t really into the whole organizer profile thing." , "2017.03.28.");
+
+    System.out.println(post0);
+    System.out.println(post1);
+    System.out.println(post2);
+
+  }
+  
+}
