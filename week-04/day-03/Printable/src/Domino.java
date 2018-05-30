6e6d761c016549be689822c2563e39c63e6ca5c1
zoldzebra <zoldzebra@gmail.com>
2018-05-30 17:46:02 +0200

    Flyable OK

---
+++ week-04/day-03/Printable/src/Domino.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Domino implements Printable{
+  private final int[] values;
+
+  public Domino(int valueA, int valueB) {
+    this.values = new int[]{valueA, valueB};
+  }
+
+  public int[] getValues() {
+    return values;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + values[0] + ", " + values[1] + "]";
+  }
+
+  @Override
+  public void printAllFields() {
+    System.out.println(this.values[0]+ ", " + this.values[1]);
+  }
+}
