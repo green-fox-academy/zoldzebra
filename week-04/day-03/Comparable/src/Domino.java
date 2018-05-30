f90f7893817cb57c0799797e7f6589cce8af2380
zoldzebra <zoldzebra@gmail.com>
2018-05-30 14:14:08 +0200

    Comparables OK, dont know exactly how :(

---
+++ week-04/day-03/Comparable/src/Domino.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Domino implements Comparable{
+  private final int[] values;
+
+  public Domino(int valueA, int valueB) {
+    this.values = new int[]{valueA, valueB};
+  }
+
+  public int[] getValues() {
+    return values;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + values[0] + ", " + values[1] + "]";
+  }
+
+  @Override
+  public int compareTo(Object o) {
+    Domino i = (Domino) o;
+    return Integer.compare(this.values[0], i.values[0]);
+  }
+}
