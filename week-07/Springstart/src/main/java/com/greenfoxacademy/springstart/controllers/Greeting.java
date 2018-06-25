ab1ff7b569e1c376e3623fe0cf0ab9803a3251d5
zoldzebra <zoldzebra@gmail.com>
2018-06-25 16:29:22 +0200

    Hello to all the World

--- week-07/Springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
+++ week-07/Springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -7,20 +7,33 @@ public class Greeting {
   String content;
   static AtomicLong idCounter = new AtomicLong();
   long id;
+  String color;
+  int fontsize;
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+  String[] colors = {"blue", "red", "green", "black", "yellow", "darkgrey"};
 
   public Greeting() {
     this.id = idCounter.incrementAndGet();
-    this.content = "Hello Worrrrld";
+    this.content = hellos[(int) (Math.random() * (hellos.length))];
+    this.color = colors[(int) (Math.random() * (colors.length))];
+    this.fontsize = 12 + ((int) (Math.random() * 40));
   }
 
   public Greeting (long id, String content) {
     this.id = idCounter.incrementAndGet();
-    this.content = content;
+    this.content = hellos[(int) (Math.random() * (hellos.length))] + " " + content;
+    this.color = colors[(int) (Math.random() * (colors.length))];
+    this.fontsize = 12 + ((int) (Math.random() * 40));
   }
 
   public Greeting (String content) {
     this.id = idCounter.incrementAndGet();
-    this.content = content;
+    this.content = hellos[(int) (Math.random() * (hellos.length))] + " " + content;
+    this.color = colors[(int) (Math.random() * (colors.length))];
+    this.fontsize = 12 + ((int) (Math.random() * 40));
   }
 
   public long getId() {
@@ -29,5 +42,13 @@ public class Greeting {
 
   public String getContent() {
     return content;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public int getFontsize() {
+    return fontsize;
   }
 }
--- week-07/Springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/Springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -9,10 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWebController {
 
   @RequestMapping("/web/greeting")
-  public String greeting(@RequestParam String name, Model model, Model model2) {
+  public String greeting(@RequestParam String name, Model model) {
     Greeting greeting = new Greeting(name);
     model.addAttribute("name", greeting.getContent());
-    model2.addAttribute("count", greeting.getId());
+    model.addAttribute("count", greeting.getId());
+    model.addAttribute("color", greeting.getColor());
+    model.addAttribute("fontsize", greeting.getFontsize());
+    System.out.println(greeting.getColor() + greeting.getContent() + greeting.getId() + greeting.getFontsize());
     return "greeting";
   }
 }
