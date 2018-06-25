c08054c5aac14b92d9ac57298cfcab394479d2eb
zoldzebra <zoldzebra@gmail.com>
2018-06-25 14:18:30 +0200

    Hello User REST app

--- week-07/Springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
+++ week-07/Springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -10,6 +10,11 @@ public class Greeting {
     this.content = "Hello Worrrrld";
   }
 
+  public Greeting (long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
+
   public long getId() {
     return id;
   }
--- week-07/Springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/Springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,14 +1,17 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
-  @RequestMapping("/greeting")
-  public Greeting greeting () {
-    Greeting greeting = new Greeting();
+  // @RequestMapping("/greeting")
+  @GetMapping("/greeting")
+  public Greeting greeting (@RequestParam String name) {
+    Greeting greeting = new Greeting(1, name);
     return greeting;
   }
 
