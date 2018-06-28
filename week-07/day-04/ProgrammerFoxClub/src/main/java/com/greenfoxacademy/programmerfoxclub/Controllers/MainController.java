b09415296003016d261bc61f85621a1d96b50a23
zoldzebra <zoldzebra@gmail.com>
2018-06-28 14:05:52 +0200

    feat(PFC): add MainController

---
+++ week-07/day-04/ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/Controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.programmerfoxclub.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+  @GetMapping ("/")
+  public String renderIndexPage() {
+    return "index";
+  }
+
+}
